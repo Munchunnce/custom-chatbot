7d32ec91abdd13ba8b78ac3ee70b04ed7aa01be6
Vimal <vimalkumarak478@gmail.com>
2025-09-08 18:38:47 +0530

    node terminal create and .gitnore file add  vkc

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log('Hello form AI');
+import readline from 'node:readline/promises';
+
+async function main() {
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+    while(true){
+        const userInput = await rl.question('You');
+        if(userInput === 'bye') break;
+        console.log('You said', userInput);
+    }
+
+    rl.close();
+};
+
+main();
